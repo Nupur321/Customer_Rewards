108b4a14634838cbead4fd5fbc54c1769125202e
Nupur321 <67397347+Nupur321@users.noreply.github.com>
2022-04-12 10:50:41 -0500

    Initial commit
    customer rewards calculated
    
    Added actuator for API Health Checks
    
    ENV variables for H2 credentials & Health API config
    
    Update README.md
    
    Update README.md
    
    Nupur321 help file deleted
    
    Nupur321 Password removed
    
    customer rewards credentials updated
    
    customer rewards
    
    customer rewards calculated
    
    customer rewards calculated
    
    customer rewards calculated

---
+++ src/main/java/com/customer/rewards/service/impl/RewardsServiceImpl.java
@@ -0,0 +1,95 @@
+package com.customer.rewards.service.impl;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+import com.customer.rewards.RewardsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.customer.rewards.model.Customer;
+import com.customer.rewards.model.Transactions;
+import com.customer.rewards.repository.CustomerRewardRepository;
+import com.customer.rewards.service.RewardsService;
+
+@Service
+public class RewardsServiceImpl implements RewardsService {
+	Logger logger = LoggerFactory.getLogger(RewardsServiceImpl.class);
+	
+	@Autowired
+	CustomerRewardRepository customerRewardRepo;
+
+	@Autowired
+	RewardsUtil rewardsUtil;
+
+	@Override
+	public List<Customer> calculateRewardsForAll() {
+		logger.info("calculateRewardsForAll method In RewardServiceImpl start....");
+
+		List<Customer> customerList = customerRewardRepo.findAll();
+		if (customerList != null) {
+			for (Customer customer : customerList) {
+				logger.debug("Calculating rewards for Each Customer:::::"
+						+ customer.getName());
+				Set<Transactions> setOfTransaction = customer.getTransactions();
+				setRewardsPerMonth(setOfTransaction, customer);
+				logger.debug("End of reward calculation for customer" + customer.getName());
+			}
+
+		}
+
+		logger.info("calculateRewardsForAll method in RewardServiceImpl ends....");
+		return customerList;
+	}
+
+	@Override
+	public Customer calculateRewardsById(Integer customerId) {
+		logger.info("calculateRewardsbyId method In RewardServiceImpl start....");
+		
+		Customer customerReward = customerRewardRepo.findById(customerId).orElse(null);
+		if(customerReward != null) {
+			logger.debug("Calculating rewards for Each Customer:::::"
+					+ customerReward.getName());
+			Set<Transactions> setOfTransaction = customerReward.getTransactions();
+			setRewardsPerMonth(setOfTransaction, customerReward);
+			logger.debug("End of reward calculation for customer" + customerReward.getName());
+		}
+		
+		logger.info("calculateRewardsbyId method In RewardServiceImpl end....");
+		return customerReward;
+	}
+	
+	private void setRewardsPerMonth(Set<Transactions> setOfTransaction, Customer customer) {
+		LocalDate todayDate = LocalDate.now();
+		
+		for (Transactions transaction : setOfTransaction) {
+			int transactionMon = transaction.getCreationDate().getMonth() + 1;
+			int transactionYear = transaction.getCreationDate().getYear() + 1900;
+			
+			
+			logger.debug("Start Calculating Rewards for Customer:::" + customer.getName() + " for Transaction Id::: "
+					+ transaction.getId());
+
+			if ((todayDate.getYear() == transactionYear) && (todayDate.getMonth().getValue() == transactionMon))
+				customer.setThirdMonthRewards(customer.getThirdMonthRewards()
+						+ rewardsUtil.calculateRewardAmountPerTrans(transaction.getTransactionAmount()));
+			
+			else if ((todayDate.minusMonths(1).getYear() == transactionYear)
+					&& (todayDate.minusMonths(1).getMonth().getValue() == transactionMon))
+				customer.setSecondMonthRewards(customer.getSecondMonthRewards()
+						+ rewardsUtil.calculateRewardAmountPerTrans(transaction.getTransactionAmount()));
+			else if ((todayDate.minusMonths(2).getYear() == transactionYear)
+					&& (todayDate.minusMonths(2).getMonth().getValue() == transactionMon))
+				customer.setFirstMonthRewards(customer.getFirstMonthRewards()
+						+ rewardsUtil.calculateRewardAmountPerTrans(transaction.getTransactionAmount()));
+
+			logger.debug("End Calculating Rewards for customer::::" + customer.getName() + " for Transaction Id:::"
+					+ transaction.getId());
+
+		}
+	}
+
+}
